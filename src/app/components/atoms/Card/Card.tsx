e2fb1f640f2c4ae5210eb1f28a05bb0ede4d54af
Lailatul Fitriyah <arwahitem01@gmail.com>
2024-04-03 20:25:53 +0700

    add molecule component

---
+++ src/app/components/atoms/Card/Card.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import { PokemonList } from "@/app/types/pokemon";
+import { bakbak_One, lalezar } from "@/app/fonts";
+import { formatId } from "@/app/utils/formatId";
+import clsx from "clsx";
+import { cardPillTheme } from "../../common/theme";
+import Logo from "../../common/Logo/Logo";
+
+
+export default function Card({ name, url, id, type }: PokemonList) {
+    return (
+        <Link
+        href={`/${name}`}
+        className={`${lalezar.className} block overflow-hidden min-h-[130px] max-h-[130px] p-4 list-none rounded-2xl capitalize font-bold text-xl shadow-lg shadow-[0_35px_60px_-15px_rgba(0,0,0,1) bg-gradient-to-r from-[#ececec] to-[#ececec] hover:to-primary motion-reduce:transition-none motion-reduce:hover:transform-none transition hover:scale-[1.1] active:scale-[0.95] animate-fade`}
+      >
+        <span className="flex flex-col gap-4">
+          <span>{name}</span>
+          <span className="flex items-center justify-between relative bg-transparent">
+            <span
+              className={clsx(
+                `${bakbak_One.className} py-1 px-2 rounded-[10px] capitalize text-xs sm:text-base z-10 font-bold`,
+                cardPillTheme(type)
+              )}
+            >
+              {formatId(id)}
+            </span>
+            <span className="absolute bottom-[-70px] right-[-30px] md:bottom-[-60px] md:right-[-20px]">
+              <Logo />
+              <Image
+                src={url}
+                alt={name}
+                objectFit="cover"
+                width={75}
+                height={75}
+                className="absolute top-0 left-0"
+              />
+            </span>
+          </span>
+        </span>
+      </Link>
+    );
+}
---
+++ src/app/components/common/CardList/CardList.tsx
@@ -0,0 +1,44 @@
+import Card from "../../atoms/Card/Card";
+import { CardGrid, CardListItem } from "../../atoms/CardGrid/CardGrid";
+
+
+import { PokemonList } from "@/app/types/pokemon";
+
+interface PokemonListProps {
+  pokemonList: PokemonList[];
+  loadMore?: boolean;
+  isError: boolean;
+  loadMorePokemon?: () => Promise<void>;
+}
+
+export default function CardList({
+  pokemonList,
+  loadMore,
+  isError,
+  loadMorePokemon,
+}: PokemonListProps) {
+  if (isError)
+    return (
+      <div className="justify-center mb-12 w-full gap-4 sm:gap-8">
+        Error Loading Pokemon
+      </div>
+    );
+  return (
+    <>
+      <CardGrid>
+        {pokemonList.map((pokemon, i) => {
+          return (
+            <CardListItem key={i}>
+              <Card
+                name={pokemon.name}
+                url={pokemon.url}
+                id={pokemon.id}
+                type={pokemon.type}
+              />
+            </CardListItem>
+          );
+        })}
+      </CardGrid>
+    </>
+  );
+}
